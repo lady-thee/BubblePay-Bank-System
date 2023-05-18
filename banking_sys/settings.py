939dcf0d9c91ed556570852aa975a3fa634418e4
web-weebie <twinklet969@gmail.com>
2023-05-19 00:40:04 +0100

    add csrf middleware

--- banking_sys/settings.py
+++ banking_sys/settings.py
@@ -59,6 +59,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+CSRF_TRUSTED_ORIGINS = ['example.com']
+
+
 ROOT_URLCONF = 'banking_sys.urls'
 
 TEMPLATES = [
